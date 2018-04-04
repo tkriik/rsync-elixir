3a37ca03985b1f7842929a2cdd0d97873fb98103
Tanel Kriik <tanel.kriik+github@gmail.com>
2018-04-04 21:25:35 +0300

    Initial commit

---
+++ lib/rsync_elixir.ex
@@ -0,0 +1,18 @@
+defmodule RsyncElixir do
+  @moduledoc """
+  Documentation for RsyncElixir.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> RsyncElixir.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/rsync_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule RsyncElixirTest do
+  use ExUnit.Case
+  doctest RsyncElixir
+
+  test "greets the world" do
+    assert RsyncElixir.hello() == :world
+  end
+end
